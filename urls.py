ed25984402807cd0cada264ab48b08cdaebe2d17
Thomas Levine <thomas.levine@gmail.com>
2011-10-15 12:24:35 -0400

    ooh

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from copy import copy
+
 from urllib2 import urlopen
 from lxml.html import fromstring
 from demjson import decode
@@ -9,12 +11,22 @@ URLS={
 }
 
 def main():
-  regions={}
+  regions=[]
+  region_id=0
   for url in region_urls():
+    region_id=region_id+1
     xml=get(URLS['base']+url['href'])
-    links=get_links(xml,'//table/tr/td/a',textkey="organization")
-    regions[url['region']]=links
-  print regions
+    links=get_links(
+      xml
+    , '//table/tr/td/a'
+    , textkey="organization"
+    , extra={"region_id":region_id}
+    )
+    regions.append({
+      "region_id":region_id
+    , "region":url['region']
+    })
+    print regions
 
 def get(url):
   raw=urlopen(url).read()
@@ -24,14 +36,14 @@ def region_urls():
   xml=get('http://www.rifidec.org/membres/infomembres.htm')
   return get_links(xml,'//a',textkey="region")[:-1]
 
-def get_links(xml,xpath,textkey="text"):
+def get_links(xml,xpath,textkey="text",extra={}):
   links=[]
   for a in xml.xpath(xpath):
     if a.text!=None:
-      links.append({
-        textkey:a.text
-      , "href":a.attrib['href']
-      })
+      row=copy(extra)
+      row[textkey]=a.text
+      row["href"]=a.attrib['href']
+      links.append(row)
   return links
 
 if __name__ == '__main__':
